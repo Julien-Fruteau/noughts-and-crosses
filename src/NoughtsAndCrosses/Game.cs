838e897cb418a7fccfeb77faf5a18dd8b193f8a8
Julien-Fruteau <julien.fruteau@gmail.com>
2020-08-13 08:24:14 +0200

    feat: complete working game

--- src/NoughtsAndCrosses/Game.cs
+++ src/NoughtsAndCrosses/Game.cs
@@ -15,25 +15,50 @@ namespace NoughtsAndCrosses
         {
             Player player = Player.Crosses;
 
-            bool @continue = true;
+            bool @continue = true;      // @ distinguishes a variable name from a C# keyword
             while (@continue)
             {
                 DisplayBoard();
                 @continue = PlayMove(player);
                 if (!@continue)
                     return;
-                
+                player = 3 - player;    // swaps between X and 0
             }
         }
 
         private bool PlayMove(Player player)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Invalid inputs quits game");
+            Console.Write($"{player}: Enter row comma column, eg. 3,3 > ");
+            string input = Console.ReadLine();
+            string[] parts = input.Split(',');
+            if (parts.Length != 2)
+                return false;
+            int.TryParse(parts[0], out int row);
+            int.TryParse(parts[1], out int column);
+
+            if (row < 1 || row > 3 || column < 1 || column > 3)
+                return false;
+
+            if (_board[row - 1][column -1].Owner != Player.None)
+            {
+                Console.WriteLine("Square is already occupied");
+                return false;
+            }
+
+            _board[row -1][column -1] = new Square(player);
+            return true;
         }
 
         private void DisplayBoard()
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < 3; i++)
+            {
+                string line = "";
+                for (int j = 0; j < 3; j++)
+                    line += " " + _board[i][j];
+                Console.WriteLine(line);
+            }
         }
     }
 }
